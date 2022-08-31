407e53dbb9a72755dce5b80b582ccce359f5fe15
Rob Foster <rjfoster1@sheffield.ac.uk>
2022-08-31 15:29:28 +0100

    Updates to ramping script

--- automation/auto_ramp.py
+++ automation/auto_ramp.py
@@ -1,8 +1,10 @@
 import sys
+import subprocess
 
 from auto_utils import monitor, cmd_sethv, ramp_and_check
 
 init_hvinfo = monitor()
+
 
 def init_hv_channels():
     '''Defines HV channel settings to match the CAEN crate.'''
@@ -16,28 +18,48 @@ def init_hv_channels():
     return channels
 
 
-hv_channels = init_hv_channels()
-detector = sys.argv[1]
-target_mcp_voltage = sys.argv[2]
+def hv_power(onoff):
+    for i, chan in enumerate(hv_channels):
+        if onoff:
+            if init_hvinfo[chan]['power'] == 'ON':
+                print('Channel ' + chan + ' power already on.')
+            else:
+                reset_cmd = f'/home/watchman/Documents/LAPPDCrate/sethv/sethv --power {chan} On'
+                subprocess.call(reset_cmd, shell=True)
+        else:
+            if init_hvinfo[chan]['power'] == 'OFF':
+                print('Channel ' + chan + ' power already off.')
+            else:
+                reset_cmd = f'/home/watchman/Documents/LAPPDCrate/sethv/sethv --power {chan} Off'
+                subprocess.call(reset_cmd, shell=True)
 
-## Check voltage won't be too high for the Photocathode for the selected LAPPD.
-v_max = 400*(target_mcp_voltage*2)
+
+hv_channels = init_hv_channels()
+detector = int(sys.argv[1])
+target_mcp_voltage = int(sys.argv[2])
+
+# Check voltage won't be too high for the Photocathode for the selected LAPPD.
+v_max = 400+(target_mcp_voltage*2)
 
 if detector in (104, 96):
     print(f"L{detector} selected.")
-    if v_max >2450:
+    if v_max > 2450:
         sys.exit("Voltage too high for chosen LAPPD.")
 else:
     sys.exit("The selected detector doesn't have ramp settings yet.")
 
 
-v_steps = list(range(200,target_mcp_voltage, 200))
+v_steps = list(range(200, target_mcp_voltage, 200))
+print("v_steps:", v_steps)
 if v_steps[-1] < target_mcp_voltage:
     v_steps.append(target_mcp_voltage)
 
-## Set up the 5 voltages for the LAPPD. If over 200 V then ramp in stages.
-## TODO: Set it to ramp back down in stages too.
-## Currently can just set all to 0 V and ramp from there.
+# Set up the 5 voltages for the LAPPD. If over 200 V then ramp in stages.
+# TODO: Set it to ramp back down in stages too.
+# Currently can just set all to 0 V and ramp from there.
+
+hv_power(1)
+
 for v_mcp in v_steps:
     if v_mcp in (0, 200):
         v_XoX = v_EoX = v_XoE = v_EoE = v_PC = v_mcp
@@ -50,13 +72,12 @@ for v_mcp in v_steps:
         v_PC = 400 + (2 * v_mcp)
     chan_volts = [v_XoX, v_EoX, v_XoE, v_EoE, v_PC]
 
-
     print(f"Setting voltages for {v_mcp} across the MCPs.")
 
-    for i,chan in enumerate(hv_channels):
+    for i, chan in enumerate(hv_channels):
         cmd_sethv(chan, chan_volts[i])
 
-    for i,chan in enumerate(hv_channels[::-1]):
+    for i, chan in enumerate(hv_channels[::-1]):
         ramp_and_check(chan)
 
     print("Voltage has been reached on all channels. The detector is ready")
