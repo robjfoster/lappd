07ff4500451a873a819e9cc395f7e74524cd2655
Rob Foster <rjfoster1@sheffield.ac.uk>
2022-09-08 11:26:33 +0100

    Add python histogramming

---
+++ lappd/utils/pyhist.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import ROOT as root
+from scipy.optimize import curve_fit
+
+
+def rootgaus(x, constant, mean, sigma):
+    return constant * np.exp(-0.5 * ((x - mean) / sigma)**2)
+
+
+def pyhist(data, fit=False, binwidth=1):
+    mindata = min(data)
+    maxdata = max(data)
+    bins = np.arange(mindata, maxdata + binwidth, binwidth)
+    plt.hist(data, bins=bins, histtype="step")
+    if fit:
+        # Fit with root, draw with python
+        tf1 = root.TF1("gaussian", "gaus", mindata, maxdata)
+        hist = root.TH1D("pyhist", "pyhist", len(bins), mindata, maxdata)
+        for value in data:
+            hist.Fill(value)
+        hist.Draw()
+        hist.Fit(tf1)
+        fitconstant = tf1.GetParameter(0)
+        fitmean = tf1.GetParameter(1)
+        fitsigma = tf1.GetParameter(2)
+        fit = rootgaus(bins, fitconstant, fitmean, fitsigma)
+        plt.plot(bins, fit)
+        del hist
+    plt.show()
